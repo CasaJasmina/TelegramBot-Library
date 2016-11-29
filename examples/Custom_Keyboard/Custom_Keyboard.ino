f3818cb7ee9eac1f3ba542050e6c96b801eea7dd
tommasoltrz <tommasoltrz@gmail.com>
2016-11-29 12:10:57 +0100

    Added watchdog

--- examples/Custom_Keyboard/Custom_Keyboard.ino
+++ examples/Custom_Keyboard/Custom_Keyboard.ino
@@ -7,18 +7,20 @@
   For a step-by-step tutorial visit:
   https://create.arduino.cc/projecthub/Arduino_Genuino/telegram-bot-library-ced4d4
 
-  Updated 19 May 2016
+  Updated 29 May 2016
   by Tommaso Laterza
 
   This example code is in the public domain.
 
 */
 
-
-#include<WiFiSSLClient.h>
 #include <WiFi101.h>
 #include <SPI.h>
 #include <TelegramBot.h>
+
+//The SleepyDog library is needed to reset the board when something goes wrong.
+//It is not strictly necessary but makes the bot way more reliable.
+#include <Adafruit_SleepyDog.h> 
 
 // Initialize Wifi connection to the router
 char ssid[] = "xxxx";             // your network SSID (name)
@@ -50,17 +52,19 @@ void setup() {
 
   // define your row's
   const char* row_one[] = {"true", "false"};
-  const char* row_two[] = {"one", "two", "thee", "hello!"};
+  const char* row_two[] = {"one", "two", "three", "hello!"};
 
   // assing a row to one or more keyboards
   // second argument is the length of the row
   keyboard_one.addRow(row_one, 2);
   keyboard_one.addRow(row_two, 4);
   bot.begin();
-
+  Watchdog.enable(10000); // set the timer to 10 sec
 }
 
 void loop() {
+
+  Watchdog.reset(); // if this function is not called within 10 seconds the board will reset itself
 
   message m = bot.getUpdates(); // Read new messages
   if ( m.chat_id != 0 ) { // Checks if there are some updates
