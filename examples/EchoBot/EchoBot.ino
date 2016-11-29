19c6a48bfedaf968e9c0feec27ab528814aed2bc
tommasoltrz <tommasoltrz@gmail.com>
2016-11-29 12:11:16 +0100

    Added watchdog

--- examples/EchoBot/EchoBot.ino
+++ examples/EchoBot/EchoBot.ino
@@ -7,16 +7,19 @@
   For a step-by-step tutorial visit:
   https://create.arduino.cc/projecthub/Arduino_Genuino/telegram-bot-library-ced4d4
 
-  Updated 19 May 2016
+  Updated 29 Nov 2016
   by Tommaso Laterza
 
   This example code is in the public domain.
 
 */
-#include<WiFiSSLClient.h>
 #include <WiFi101.h>
 #include <SPI.h>
 #include <TelegramBot.h>
+
+//The SleepyDog library is needed to reset the board when something goes wrong.
+//It is not strictly necessary but makes the bot way more reliable.
+#include <Adafruit_SleepyDog.h> 
 
 // Initialize Wifi connection to the router
 char ssid[] = "xxxx";             // your network SSID (name)
@@ -49,10 +52,13 @@ void setup() {
 
 
   bot.begin();
+  Watchdog.enable(10000); // set the timer to 10 sec
 
 }
 
 void loop() {
+  
+    Watchdog.reset(); // if this function is not called within 10 seconds the board will reset itself
 
     message m = bot.getUpdates(); // Read new messages
     if ( m.chat_id != 0 ){ // Checks if there are some updates
